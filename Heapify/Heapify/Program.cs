6438d041c40705462c0b3bbcaf4871d4e1e95fa5
Nitzan Selwyn <nitzans@sela.co.il>
2020-01-19 10:00:38 +0200

    init

---
+++ Heapify/Heapify/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Heapify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 3, 8, 4, 1, 2 };
+            Heapify(numbers);
+            Console.WriteLine(numbers.ToString());
+        }
+
+        public static void Heapify(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Heapify(arr, i);
+            }
+        }
+
+        private static void Heapify(int[] arr, int index)
+        {
+            var largerIndex = index;
+
+            var leftIndex = index * 2 + 1;
+            if (leftIndex < arr.Length && arr[leftIndex] > arr[largerIndex])
+            {
+                largerIndex = leftIndex;
+            }
+
+            var rightIndex = index * 2 + 2;
+            if (rightIndex < arr.Length && arr[rightIndex] > arr[largerIndex])
+            {
+                largerIndex = rightIndex;
+            }
+
+            if (index == largerIndex)
+                return;
+
+            Sawp(arr, index, largerIndex);
+            Heapify(arr, largerIndex);
+        }
+
+        private static void Sawp(int[] arr, int first, int second)
+        {
+            var temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
